0e9ed81e15c037e98f6b6205e51b9950934aad6f
Pablo Ferrari <55794722+PabloFerrari013@users.noreply.github.com>
2023-01-27 11:04:09 -0300

    trying to find the problem with the chat

--- src/components/user/Chat/index.tsx
+++ src/components/user/Chat/index.tsx
@@ -104,6 +104,8 @@ const Chat: React.FC = () => {
       socket?.emit('read', 'admin')
 
       socket?.on('messages', (data: Message[]) => {
+        console.log(data)
+
         handleSocketMessages()
       })
 
